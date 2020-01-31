8a26e71445cdab0b97c6577a88dea8a48d2b12a4
Izabela <Suporte-2@DESKTOP-NA9EGVV>
2020-01-31 17:29:08 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+namespace RequisicaoSimplesWeb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var requisicaoWeb = WebRequest.CreateHttp("https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=143f8e4b323931be4ab6d40856903ea546e4a9e8");
+            requisicaoWeb.Method = "GET";
+            requisicaoWeb.UserAgent = "RequisicaoWebDemo";
+            using (var resposta = requisicaoWeb.GetResponse())
+            {
+                var streamDados = resposta.GetResponseStream();
+                StreamReader reader = new StreamReader(streamDados);
+                object objResponse = reader.ReadToEnd();
+                Console.WriteLine(objResponse.ToString());
+                Console.ReadLine();
+                streamDados.Close();
+                resposta.Close();
+            }
+
+        }
+    }
+}
